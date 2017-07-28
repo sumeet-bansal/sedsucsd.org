ed30817a5dc69ce1f775c0f1fa379ee46bf4f774
Albert Lin <albertxp@gmail.com>
2017-07-28 12:37:18 -0700

    fixed wow.min.js link

--- assets/blocks/js_footer.php
+++ assets/blocks/js_footer.php
@@ -5,33 +5,33 @@
  *
  */
 
-echo '<!-- JS --> \n';
-echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/jquery-1.11.2.min.js"></script> \n';
-echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/jquery.easing.1.3.js"></script> \n';
-echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/jquery.scrollTo.min.js"></script> \n';
-echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/jquery.localScroll.min.js"></script> \n';
-echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/jquery.viewport.mini.js"></script> \n';
-echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/jquery.appear.js"></script> \n';
-echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/jquery.sticky.js"></script> \n';
-echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/jquery.parallax-1.1.3.js"></script> \n';
-echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/owl.carousel.min.js"></script> \n';
-echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/isotope.pkgd.min.js"></script> \n';
-echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/imagesloaded.pkgd.min.js"></script> \n';
-echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/jquery.magnific-popup.min.js"></script> \n';
-echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/wow.min.js.js"></script> \n';
-echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/masonry.pkgd.min.js"></script> \n';
-echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/all.js"></script> \n';
+echo '<!-- JS -->';
+echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/jquery-1.11.2.min.js"></script>' \n;
+echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/jquery.easing.1.3.js"></script>';
+echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/jquery.scrollTo.min.js"></script>';
+echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/jquery.localScroll.min.js"></script>';
+echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/jquery.viewport.mini.js"></script>';
+echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/jquery.appear.js"></script>';
+echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/jquery.sticky.js"></script>';
+echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/jquery.parallax-1.1.3.js"></script>';
+echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/owl.carousel.min.js"></script>';
+echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/isotope.pkgd.min.js"></script>';
+echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/imagesloaded.pkgd.min.js"></script>';
+echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/jquery.magnific-popup.min.js"></script>';
+echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/wow.min.js"></script>';
+echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/masonry.pkgd.min.js"></script>';
+echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/all.js"></script>';
 echo '	<!--[if lt IE 10]><script type="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'assets/js/placeholder.js"></script><![endif]-->';
 
 /*
-echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/SmoothScroll.js"></script> \n';
-echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/jquery.countTo.js"></script> \n';
-echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/jquery.fitvids.js.js"></script> \n';
-echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/jquery.mb.YTPlayer.js"></script> \n';
-echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/jquery.ajaxchimp.min.js"></script> \n';
-echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/contact-form.js"></script> \n';
-echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/jquery.simple-text-rotator.min.js"></script> \n';
-echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/gmap3.min.js"></script> \n';
+echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/SmoothScroll.js"></script>';
+echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/jquery.countTo.js"></script>';
+echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/jquery.fitvids.js.js"></script>';
+echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/jquery.mb.YTPlayer.js"></script>';
+echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/jquery.ajaxchimp.min.js"></script>';
+echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/contact-form.js"></script>';
+echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/jquery.simple-text-rotator.min.js"></script>';
+echo '<script tyle="text/javascript" src="//'.$_SERVER["SERVER_NAME"].'/assets/js/gmap3.min.js"></script>';
 */
 
 
