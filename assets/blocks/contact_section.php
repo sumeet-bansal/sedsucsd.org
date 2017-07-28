d3eed099ddfff1a8eb5b3f73df07a91baa20b875
Albert Lin <albertxp@gmail.com>
2017-07-28 12:41:15 -0700

    contact section block

---
+++ assets/blocks/contact_section.php
@@ -0,0 +1,21 @@
+<?php
+/* Contact Section
+ *
+ * To include on a page, use <?php include($_SERVER["DOCUMENT_ROOT"].'/assets/blocks/contact_section.php') ;?>
+ *
+ */
+ ?>
+
+<!-- Contact Section -->
+<section class="small-section bg-dark pt-20 pb-40" style="background-color: #0E76BC;">
+	<div class="container relative">                   
+		<div class="align-center">
+			<h3>Questions, concerns, or just curious?</h3>
+				<div>
+					<a href="mailto:seds.ucsds@gmail.com" class="btn btn-mod btn-w btn-medium btn-round"><i class="fa fa-envelope"></i> &nbsp; Contact Us</a>
+				</div>
+		</div>
+	</div>
+</section>
+<!-- End Contact Section -->
+
--- projects/engines/index.php
+++ projects/engines/index.php
@@ -336,21 +336,10 @@
             </section>
 
             
-			<!-- Call Action Section -->
-             <section class="small-section bg-dark pt-20 pb-40" style="background-color: #0E76BC;">
-                <div class="container relative">                   
-                    <div class="align-center">
-                        <h3>Questions, concerns, or just curious?</h3>
-                        <div>
-                            <a href="mailto:seds.ucsds@gmail.com" class="btn btn-mod btn-w btn-medium btn-round"><i class="fa fa-envelope"></i> &nbsp; Contact Us</a>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <!-- End Call Action Section -->
+			
 
 
-
+            <?php include($_SERVER["DOCUMENT_ROOT"].'/assets/blocks/contact_section.php') ;?>
             <?php include($_SERVER["DOCUMENT_ROOT"].'/assets/blocks/footer.php') ;?>
 
 
