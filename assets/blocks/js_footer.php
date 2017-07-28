d4e8b7f004a9c86a8f4ce99e1acbfce05779115b
Albert Lin <albertxp@gmail.com>
2017-07-28 12:25:22 -0700

    fixed js links

--- assets/blocks/js_footer.php
+++ assets/blocks/js_footer.php
@@ -7,30 +7,30 @@
  ?>
 
 	<!-- JS -->
-	<script type="text/javascript" src="../assets/js/jquery-1.11.2.min.js"></script>
-	<script type="text/javascript" src="../assets/js/jquery.easing.1.3.js"></script>
-	<script type="text/javascript" src="../assets/js/bootstrap.min.js"></script>
-	<!--<script type="text/javascript" src="assets/js/SmoothScroll.js"></script>-->
-	<script type="text/javascript" src="../assets/js/jquery.scrollTo.min.js"></script>
-	<script type="text/javascript" src="../assets/js/jquery.localScroll.min.js"></script>
-	<script type="text/javascript" src="../assets/js/jquery.viewport.mini.js"></script>
-	<!--<script type="text/javascript" src="assets/js/jquery.countTo.js"></script>-->
-	<script type="text/javascript" src="../assets/js/jquery.appear.js"></script>
-	<script type="text/javascript" src="../assets/js/jquery.sticky.js"></script>
-	<script type="text/javascript" src="../assets/js/jquery.parallax-1.1.3.js"></script>
-	<!--<script type="text/javascript" src="../assets/js/jquery.fitvids.js"></script>-->
-	<script type="text/javascript" src="../assets/js/owl.carousel.min.js"></script>
-	<script type="text/javascript" src="../assets/js/isotope.pkgd.min.js"></script>
-	<script type="text/javascript" src="../assets/js/imagesloaded.pkgd.min.js"></script>
-	<script type="text/javascript" src="../assets/js/jquery.magnific-popup.min.js"></script>
+	<script type="text/javascript" src="/assets/js/jquery-1.11.2.min.js"></script>
+	<script type="text/javascript" src="/assets/js/jquery.easing.1.3.js"></script>
+	<script type="text/javascript" src="/assets/js/bootstrap.min.js"></script>
+	<!--<script type="text/javascript" src="/assets/js/SmoothScroll.js"></script>-->
+	<script type="text/javascript" src="/assets/js/jquery.scrollTo.min.js"></script>
+	<script type="text/javascript" src="/assets/js/jquery.localScroll.min.js"></script>
+	<script type="text/javascript" src="/assets/js/jquery.viewport.mini.js"></script>
+	<!--<script type="text/javascript" src="/assets/js/jquery.countTo.js"></script>-->
+	<script type="text/javascript" src="/assets/js/jquery.appear.js"></script>
+	<script type="text/javascript" src="/assets/js/jquery.sticky.js"></script>
+	<script type="text/javascript" src="/assets/js/jquery.parallax-1.1.3.js"></script>
+	<!--<script type="text/javascript" src="/assets/js/jquery.fitvids.js"></script>-->
+	<script type="text/javascript" src="/assets/js/owl.carousel.min.js"></script>
+	<script type="text/javascript" src="/assets/js/isotope.pkgd.min.js"></script>
+	<script type="text/javascript" src="/assets/js/imagesloaded.pkgd.min.js"></script>
+	<script type="text/javascript" src="/assets/js/jquery.magnific-popup.min.js"></script>
 	<!--<script type="text/javascript" src="assets/js/gmap3.min.js"></script>
 	<script type="text/javascript" src="assets/js/wow.min.js"></script>-->
-	<script type="text/javascript" src="../assets/js/masonry.pkgd.min.js"></script>
+	<script type="text/javascript" src="/assets/js/masonry.pkgd.min.js"></script>
 	<!--<script type="text/javascript" src="assets/js/jquery.simple-text-rotator.min.js"></script>-->
-	<script type="text/javascript" src="../assets/js/all.js"></script>
-	<!--<script type="text/javascript" src="assets/js/contact-form.js"></script>-->
-	<!--<script type="text/javascript" src="assets/js/jquery.ajaxchimp.min.js"></script>-->
-	<!--<script type="text/javascript" src="../assets/js/jquery.mb.YTPlayer.js"></script>-->
+	<script type="text/javascript" src="/assets/js/all.js"></script>
+	<!--<script type="text/javascript" src="/assets/js/contact-form.js"></script>-->
+	<!--<script type="text/javascript" src="/assets/js/jquery.ajaxchimp.min.js"></script>-->
+	<!--<script type="text/javascript" src="/assets/js/jquery.mb.YTPlayer.js"></script>-->
 	<!--[if lt IE 10]><script type="text/javascript" src="assets/js/placeholder.js"></script><![endif]-->
 
 
