402711fa62919b972ed0c632a44f40d2acc56b0d
Albert Lin <albertxp@gmail.com>
2017-07-28 13:49:13 -0700

    vulcan page

--- assets/blocks/css_header.php
+++ assets/blocks/css_header.php
@@ -6,14 +6,14 @@
  */
  ?>
 
-	<!-- CSS -->
-	<link rel="stylesheet" href="/assets/css/bootstrap.min.css">
-	<link rel="stylesheet" href="/assets/css/style.css">
-	<link rel="stylesheet" href="/assets/css/style-responsive.css">
-	<link rel="stylesheet" href="/assets/css/animate.min.css">
-	<link rel="stylesheet" href="/assets/css/vertical-rhythm.min.css">
-	<link rel="stylesheet" href="/assets/css/owl.carousel.css">
-	<link rel="stylesheet" href="/assets/css/magnific-popup.css">
-	<!--<link rel="stylesheet" href="/assets/css/YTPlayer.css">-->
-	<link rel="stylesheet" href="/assets/css/margin-padding-text.css">
+		<!-- CSS -->
+		<link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/assets/css/style.css">
+		<link rel="stylesheet" href="/assets/css/style-responsive.css">
+		<link rel="stylesheet" href="/assets/css/animate.min.css">
+		<link rel="stylesheet" href="/assets/css/vertical-rhythm.min.css">
+		<link rel="stylesheet" href="/assets/css/owl.carousel.css">
+		<link rel="stylesheet" href="/assets/css/magnific-popup.css">
+		<!--<link rel="stylesheet" href="/assets/css/YTPlayer.css">-->
+		<link rel="stylesheet" href="/assets/css/margin-padding-text.css">
 
---
+++ assets/js/projects-vulcan-1-toggle.js
@@ -0,0 +1,65 @@
+//Avionics 
+$(document).ready(function(){
+			$("#avionics-toggle").click(function(){
+			$("#avionics-subdescription").toggle();
+			});
+			});
+
+//Aeroshell		
+$(document).ready(function(){
+			$("#aero-toggle").click(function(){
+			$("#aeroshell-subdescription").toggle();
+			});
+			});
+
+//Valve (Plumbing/Pneumatics)
+$(document).ready(function(){
+			$("#valve-toggle").click(function(){
+			$("#valve-subdescription").toggle();
+			});
+			});
+
+//Engine
+$(document).ready(function(){
+			$("#engine-toggle").click(function(){
+			$("#engine-subdescription").toggle();
+			});
+			});
+
+//Recovery
+$(document).ready(function(){
+			$("#recovery-toggle").click(function(){
+			$("#recovery-subdescription").toggle();
+			});
+			});
+
+//Tanks
+$(document).ready(function(){
+			$("#tanks-toggle").click(function(){
+			$("#tanks-subdescription").toggle();
+			});
+			});
+
+//Fins
+$(document).ready(function(){
+			$("#fins-toggle").click(function(){
+			$("#fins-subdescription").toggle();
+			});
+			});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
